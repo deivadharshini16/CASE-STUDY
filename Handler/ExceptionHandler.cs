1c8281c6962158e487cf3c4017fc6670fd1f4217
deivadharshini16 <105850635+deivadharshini16@users.noreply.github.com>
2023-03-13 10:27:42 +0530

    Add files via upload

---
+++ Handler/ExceptionHandler.cs
@@ -0,0 +1,46 @@
+﻿using GoToMeetingApp.Handler;
+using GoToMeetingApp.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using NLog;
+using System.Net;
+using System.Threading.Tasks;
+using System;
+using GoToMeetingApp.Logger;
+
+namespace GoToMeetingApp.Handler
+{
+    public class ExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILoggerService _logger;
+        public ExceptionHandler(RequestDelegate next, ILoggerService logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong: {ex}");
+                await HandleException(httpContext, ex);
+            }
+        }
+        private async Task HandleException(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await context.Response.WriteAsync(new ErrorInfo()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error from the custom middleware."
+            }.ToString());
+        }
+    }
+}
+

80d99e2ff723fc073648b2f6cf580cd787cfa97a
deivadharshini16 <105850635+deivadharshini16@users.noreply.github.com>
2023-03-13 10:31:43 +0530

    Add files via upload

---
+++ ActionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Linq;
+
+namespace GoToMeetingApp
+{
+    public class ActionFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine("before execution");
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine("After Execution");
+        }
+    }
+}

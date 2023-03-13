a7120a55829d69088955b25832b98e5a4f3c57eb
deivadharshini16 <105850635+deivadharshini16@users.noreply.github.com>
2023-03-13 10:29:37 +0530

    Add files via upload

---
+++ Logger/ILoggerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using GoToMeetingApp.Handler;
+namespace GoToMeetingApp.Logger
+{
+    public interface ILoggerService
+    {
+        void LogInfo(string message);
+        void LogWarn(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+        void LogTrace(string message);
+
+    }
+}
